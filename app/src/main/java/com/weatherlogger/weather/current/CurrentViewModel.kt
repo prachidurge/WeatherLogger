fcd30c18d37c3c86d288dd360f99e75e7174ee59
prachidurge <prachi.durge@gmail.com>
2020-07-13 10:54:06 +0530

    Working with Android Studio 4.2 canary

--- app/src/main/java/com/weatherlogger/weather/current/CurrentViewModel.kt
+++ app/src/main/java/com/weatherlogger/weather/current/CurrentViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.weatherlogger.data.entity.Main
 import com.weatherlogger.data.repository.WeatherRepository
-import com.weatherlogger.utilities.lazyDeferred
 
 class CurrentViewModel(
     private val weatherRepository: WeatherRepository
